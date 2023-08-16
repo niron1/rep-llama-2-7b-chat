9291877d1ad013f479284b1eb9ffd5c668193bcc
Nir Oren <niro@edev.co>
2023-08-16 21:34:48 +0300

    streamer

--- predict.py
+++ predict.py
@@ -2,12 +2,12 @@
 # temperature has effect in this version
 # streaming support
 
-from cog import BasePredictor, Input
+from cog import BasePredictor, Input, ConcatenateIterator
 import torch
-from transformers import AutoTokenizer, AutoModelForCausalLM, TextStreamer
+from transformers import AutoTokenizer, AutoModelForCausalLM, TextIteratorStreamer
 import re
 from datetime import datetime
-
+from threading import Thread
 
 class Predictor(BasePredictor):
     def setup(self) -> None:
@@ -37,15 +37,22 @@ class Predictor(BasePredictor):
         max_new_tokens: int = Input(description="max_new_tokens", default=1000 ),
         temperature: float = Input(description="temperature", default=0.9 ),
         repetition_penalty: float = Input(description="repetition_penalty", default=1.1),
-    ) -> str:
+    ) -> ConcatenateIterator[str]:
         inputs = self.tokenizer.encode(prompt, return_tensors="pt").to(self.device)
 
-        streamer = TextStreamer(self.tokenizer)
+        streamer = TextIteratorStreamer(self.tokenizer)
         seed = int(datetime.now().timestamp())
         print("seed", seed)
         torch.manual_seed(seed)
-        outputs = self.model.generate(inputs, max_new_tokens=max_new_tokens,temperature=temperature, repetition_penalty=repetition_penalty, streamer=streamer, do_sample=True)
-        output = self.tokenizer.decode(outputs[0])
-        no_padding = re.sub(r'<s> |</s>', '', output)
-        unique_output = no_padding[len(prompt):]
-        return unique_output.strip()
+        generation_kwargs = dict(inputs, max_new_tokens=max_new_tokens,temperature=temperature, repetition_penalty=repetition_penalty, streamer=streamer, do_sample=True)
+        thread = Thread(target=self.model.generate, kwargs=generation_kwargs)
+        thread.start()
+        generated_text = ""
+        for new_text in streamer:
+            generated_text += new_text
+            yield generated_text
+
+        # output = self.tokenizer.decode(outputs[0])
+        # no_padding = re.sub(r'<s> |</s>', '', output)
+        # unique_output = no_padding[len(prompt):]
+        # return unique_output.strip()
